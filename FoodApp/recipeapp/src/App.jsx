2f551a1d10cbd8112fa0332c63de5bcf164d5a06
Sujitmaurya123 <sujitkic6802maurya@gmail.com>
2023-11-09 20:57:44 +0530

    Make A FoodApp

---
+++ FoodApp/recipeapp/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+import Featured from './components/Featured'
+import TopNav from './components/TopNav'
+
+function App() {
+ 
+
+  return (
+    <div>
+       <TopNav/>
+       <Featured/>
+    </div>
+  )
+}
+
+export default App
